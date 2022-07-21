865a680d3de347600423cffebae5eab5d9c95d36
Kyle Tillson <kyletillson004@gmail.com>
2022-07-21 10:12:08 -0600

    commit tree 55ef767431d8a2147d8b79a89fb2ee5e55cb80e3

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(names, thankYou) {
+    const cards = []
+    for (let i = 0; i < names.length; i++) {
+        cards.push(`Thank you, ${names[i]}, for the wonderful ${thankYou} gift!`)
+    }
+    return cards
+}  
+function countDown( starting ) {
+    while ( starting > 0 ) {
+      console.log( starting );
+      starting -= 1;
+    }
+    console.log( starting );
+  }
+
